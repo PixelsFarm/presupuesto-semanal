24cbc2a6da62d0c5d4949ece5782e60f3dc491f7
hector <h.pascual@ondho.com>
2020-11-10 23:15:22 +0100

    Restar al presupuesto cada vez que hagamos un gasto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pregunta from './components/Pregunta'
 import Formulario from './components/Formulario'
 import Listado from './components/Listado'
@@ -19,13 +19,38 @@ function App() {
     //! state gastos
     const [gastos, guardarGastos] = useState([])
 
+    //! state gasto
+    const [gasto, guardarGasto] = useState({})
+
+    //! state crear gasto
+    const [creargasto, guardarCrearGasto] = useState(false)
+
     //! funcion cada vez agreguemos nuevo gasto
-    const agregarNuevoGasto = gasto => {
+    //const agregarNuevoGasto = gasto => {
+        // guardarGastos([
+        //     ...gastos,
+        //     gasto
+        // ])
+    //}
+
+    //! useffect que actualiza el restante
+    useEffect(() => {
+        //* Sólo guarda si se ha añadido gasto. Previene que al iniciar salga en listado vacío+$
+        if (!creargasto) return
+
+
         guardarGastos([
             ...gastos,
             gasto
         ])
-    }
+
+        //* Resta presupuesto actual
+        const presupuestoRestante = restante - gasto.cantidad
+        guardarRestante(presupuestoRestante)
+
+        //* resetear a false
+        guardarCrearGasto(false)
+    }, [gasto]);
 
     return (
         <div className="container">
@@ -43,7 +68,9 @@ function App() {
                             <div className="row">
                                 <div className="one-half column">
                                     <Formulario
-                                        agregarNuevoGasto={agregarNuevoGasto}   
+                                        //agregarNuevoGasto={agregarNuevoGasto}
+                                        guardarGasto={guardarGasto}
+                                        guardarCrearGasto={guardarCrearGasto}
                                     />
                                 </div>
                                 <div className="one-half column">
